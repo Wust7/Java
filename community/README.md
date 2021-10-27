96ba192e721a9cfc24861a27ee3320df818a7c83
Wust <Wust@cug.edu.cn>
2021-10-27 20:47:11 +0800

    上传目录

---
+++ community/README.md
@@ -0,0 +1,6 @@
+## springboot基础知识
+springboot是通过容器管理bean的，所有带有注解的文件，只要在当前application的同一级或者下一级目录都可以自动加载进来
+
+## git命令
+1.当发现上次commit有错误的时候可使用命令git commit --amend --no-edit进行追加，且不需要修改修改上传注释
+2.所有commit之前一定要add
---
+++ community/src/main/java/wust/community/controller/HelloContoller.java
@@ -0,0 +1,16 @@
+package wust.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//controller意为允许该类接收前端请求
+@Controller
+public class HelloContoller {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";//如果返回hello，则自动去模板中找hello的模板
+    }
+}
