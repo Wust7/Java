158aa099dab131b07fb243f9f26dd7d901f6eb5c
Wust <Wust@cug.edu.cn>
2021-10-28 22:01:21 +0800

    获取用户信息有问题，未修改

--- community/src/main/java/wust/community/controller/AuthorizeController.java
+++ community/src/main/java/wust/community/controller/AuthorizeController.java
@@ -1,17 +1,37 @@
 package wust.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import wust.community.dto.AccessTokenDO;
+import wust.community.provider.GithubProvider;
 
 /**
  * 授权页面
  */
 @Controller
 public class AuthorizeController {
+    /**
+     * 通过Component注解将provider放到了容器里面，通过Autowired使用
+     * @param code
+     * @param state
+     * @return
+     */
+
+    @Autowired
+    private GithubProvider githubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
+//        AccessTokenDO accessTokenDO = new AccessTokenDO();
+//        accessTokenDO.setCode(code);
+//        accessTokenDO.setState(state);
+//        accessTokenDO.setClient_id("Iv1.e67e5d34c9d44454");
+//        accessTokenDO.setClient_secre("f145e8349e2a32215e81588b3c1bf00f6cbf2a44");
+//        accessTokenDO.setRedirect_uri("http://localhost:8081/callback");
+//        githubProvider.getAccessToken(accessTokenDO);
         return "index";
     }
 }
--- community/src/main/java/wust/community/provider/GithubProvider.java
+++ community/src/main/java/wust/community/provider/GithubProvider.java
@@ -1,8 +1,10 @@
 package wust.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 import wust.community.dto.AccessTokenDO;
+import wust.community.dto.GithubUser;
 
 import java.io.IOException;
 
@@ -13,20 +15,37 @@ import java.io.IOException;
 @Component
 public class GithubProvider {
     public String getAccessToken(AccessTokenDO accessTokenDO){
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(JSON, json);
+//        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDO));
+        RequestBody body=RequestBody.Companion.create(JSON.toJSONString(accessTokenDO),mediaType);
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
-        try (Response response = client.newCall(request).execute()) {
+        try {
+            Response response = client.newCall(request).execute();
             String string = response.body().string();
             System.out.println(string);
             return string;
         } catch (IOException e) {
+        }
+        return null;
+    }
+    public GithubUser getUser(String access_token){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user")
+                .header("Authorization","token "+access_token)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
+        return null;
     }
 }
